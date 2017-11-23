f0f5e61debcb548c0a5b4a1ebb2127598d370659
Thiago Takeda <hdkk.tk@gmail.com>
2017-11-23 01:59:21 -0200

    CARALHO DE RADIO BOXXXXXXXXXXXXXXXX

--- MonsterLab-Carty-Jekyll/js/javascript.js
+++ MonsterLab-Carty-Jekyll/js/javascript.js
@@ -3,9 +3,37 @@ $('.product-cs').carousel({
 });
 
 $('.btn-pcsac').click(function() {
-    $('#cartybox-container .ps-slide').hide();
+    $('#products-container .ps-slide').hide();
     var target = '#' + $(this).data('target');
     $(target).show();
     $('.btn-pcsac').removeClass("active");
     $(this).addClass("active");
 })
+
+function CustomSelect() {
+    var container = ".custom-select",
+        selected_selector = ".custom-select-selected",
+        obj = $(container);
+
+    if (obj.length) {
+        var selected = obj.find(selected_selector);
+        var options = obj.find("ul").children();
+
+        $(this).css("z-index", parseInt(999));
+
+        $(selected).click(function() {
+            options.parent().toggle();
+        });
+
+        $.each(options, function(k1, v1) {
+            $(this).click(function() {
+                selected.html($(this).html());
+                options.parent().toggle();
+            });
+        });
+    }
+}
+
+$().ready(function() {
+    CustomSelect();
+});
